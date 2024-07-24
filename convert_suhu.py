e2bef8c545979d150a4adedf36d6c119a8806b04
rahmathr <rahmathr.king@gmail.com>
2024-07-24 16:41:06 +0700

    update

--- convert_suhu.py
+++ convert_suhu.py
@@ -51,86 +51,90 @@ def Kelvin(input_nilai):
   return kelvin, celcius, fahrenheit, reamur, rankine
 
 # Program Akan Berjalan Terus Menerus
-while True:
-  os.system("cls")
-  
-  # Banner
-  print("="*30)
-  print("*** Konversi Satuan Suhu🌡️  ***")
-  print("="*30)
-  
-  # Input Nilai
-  input_nilai = float(input("Input Nilai       : "))
-  print("="*30)
-  
-  print("1. Celcius")
-  print("2. Fahrenheit")
-  print("3. Reamur")
-  print("4. Rankine")
-  print("5. Kelvin")
-  print("="*30)
-  # print("0. Keluar Program")
-  # print("="*28)
-  
-  # Input Satuan
-  pilih_satuan = int(input("Pilih Satuan      : "))
-  print("="*30)
+def main():
+  while True:
+    os.system("cls")
+    
+    # Banner
+    print("="*30)
+    print("*** Konversi Satuan Suhu🌡️  ***")
+    print("="*30)
+    
+    # Input Nilai
+    input_nilai = float(input("Input Nilai       : "))
+    print("="*30)
+    
+    print("1. Celcius")
+    print("2. Fahrenheit")
+    print("3. Reamur")
+    print("4. Rankine")
+    print("5. Kelvin")
+    print("="*30)
+    # print("0. Keluar Program")
+    # print("="*28)
+    
+    # Input Satuan
+    pilih_satuan = int(input("Pilih Satuan      : "))
+    print("="*30)
 
-  if pilih_satuan == 1:
-    celcius, fahrenheit, kelvin, reamur, rankine = Celcius(input_nilai=input_nilai)
-    print(f"Celcius    = {celcius:.2f}°C")
-    print(f"Fahrenheit = {fahrenheit:.2f}°F")
-    print(f"kelvin     = {kelvin:.2f}K")
-    print(f"Reamur     = {reamur:.2f}°R")
-    print(f"Rankine    = {rankine:.2f}°Ra") 
-    print("")
-    sleep(5)
+    if pilih_satuan == 1:
+      celcius, fahrenheit, kelvin, reamur, rankine = Celcius(input_nilai=input_nilai)
+      print(f"Celcius    = {celcius:.2f}°C")
+      print(f"Fahrenheit = {fahrenheit:.2f}°F")
+      print(f"kelvin     = {kelvin:.2f}K")
+      print(f"Reamur     = {reamur:.2f}°R")
+      print(f"Rankine    = {rankine:.2f}°Ra") 
+      print("")
+      sleep(5)
+      
+    elif pilih_satuan == 2:
+      fahrenheit, celcius, kelvin, reamur, rankine = Fahrenheit(input_nilai=input_nilai)
+      print(f"Fahrenheit = {fahrenheit:.2f}°F")
+      print(f"Celcius    = {celcius:.2f}°C")
+      print(f"kelvin     = {kelvin:.2f}K")
+      print(f"Reamur     = {reamur:.2f}°R")
+      print(f"Rankine    = {rankine:.2f}°Ra")
+      print("")
+      sleep(5)
+      
+    elif pilih_satuan == 3:
+      reamur, celcius, fahrenheit, kelvin, rankine = Reamur(input_nilai=input_nilai)
+      print(f"Fahrenheit = {fahrenheit:.2f}°F")
+      print(f"Celcius    = {celcius:.2f}°C")
+      print(f"kelvin     = {kelvin:.2f}K")
+      print(f"Reamur     = {reamur:.2f}°R")
+      print(f"Rankine    = {rankine:.2f}°Ra")
+      print("")
+      sleep(5)
+      
+    elif pilih_satuan == 4:
+      rankine, celcius, fahrenheit, kelvin, reamur  = Rankine(input_nilai=input_nilai)
+      print(f"Rankine    = {rankine:.2f}°Ra")
+      print(f"Celcius    = {celcius:.2f}°C")
+      print(f"Fahrenheit = {fahrenheit:.2f}°F")
+      print(f"kelvin     = {kelvin:.2f}K")
+      print(f"Reamur     = {reamur:.2f}°R")
+      print("")
+      sleep(5)
+      
+    elif pilih_satuan == 5:
+      kelvin, celcius, fahrenheit, reamur, rankine  = Kelvin(input_nilai=input_nilai)
+      print(f"kelvin     = {kelvin:.2f}K")
+      print(f"Celcius    = {celcius:.2f}°C")
+      print(f"Fahrenheit = {fahrenheit:.2f}°F")
+      print(f"Reamur     = {reamur:.2f}°R")
+      print(f"Rankine    = {rankine:.2f}°Ra")
+      print("")
+      sleep(5)
+      
+    # elif pilih_satuan == 0:
+    #   break
     
-  elif pilih_satuan == 2:
-    fahrenheit, celcius, kelvin, reamur, rankine = Fahrenheit(input_nilai=input_nilai)
-    print(f"Fahrenheit = {fahrenheit:.2f}°F")
-    print(f"Celcius    = {celcius:.2f}°C")
-    print(f"kelvin     = {kelvin:.2f}K")
-    print(f"Reamur     = {reamur:.2f}°R")
-    print(f"Rankine    = {rankine:.2f}°Ra")
-    print("")
-    sleep(5)
+    else:
+      print("")
+      print("Pilihan yang Anda masukkan salah!, Silahkan ulangi kembali 🔁")
+      sleep(5)
+      continue
     
-  elif pilih_satuan == 3:
-    reamur, celcius, fahrenheit, kelvin, rankine = Reamur(input_nilai=input_nilai)
-    print(f"Fahrenheit = {fahrenheit:.2f}°F")
-    print(f"Celcius    = {celcius:.2f}°C")
-    print(f"kelvin     = {kelvin:.2f}K")
-    print(f"Reamur     = {reamur:.2f}°R")
-    print(f"Rankine    = {rankine:.2f}°Ra")
-    print("")
-    sleep(5)
-    
-  elif pilih_satuan == 4:
-    rankine, celcius, fahrenheit, kelvin, reamur  = Rankine(input_nilai=input_nilai)
-    print(f"Rankine    = {rankine:.2f}°Ra")
-    print(f"Celcius    = {celcius:.2f}°C")
-    print(f"Fahrenheit = {fahrenheit:.2f}°F")
-    print(f"kelvin     = {kelvin:.2f}K")
-    print(f"Reamur     = {reamur:.2f}°R")
-    print("")
-    sleep(5)
-    
-  elif pilih_satuan == 5:
-    kelvin, celcius, fahrenheit, reamur, rankine  = Kelvin(input_nilai=input_nilai)
-    print(f"kelvin     = {kelvin:.2f}K")
-    print(f"Celcius    = {celcius:.2f}°C")
-    print(f"Fahrenheit = {fahrenheit:.2f}°F")
-    print(f"Reamur     = {reamur:.2f}°R")
-    print(f"Rankine    = {rankine:.2f}°Ra")
-    print("")
-    sleep(5)
-    
-  # elif pilih_satuan == 0:
-  #   break
-  
-  else:
-    print("")
-    print("Pilihan yang Anda masukkan salah!")
-    sleep(5)
-    continue
+if __name__ == "__main__":
+  main()
